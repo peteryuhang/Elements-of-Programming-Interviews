9cd083d728f4ee3b0088d33b6a2bcb89691404c6
Yuhang Peng <peter.yuhangpeng@gmail.com>
2023-11-01 23:22:43 -0700

    Add variant solution for 6.2

--- problems/arrays/IncrementAnArbitraryPrecisionInt.java
+++ problems/arrays/IncrementAnArbitraryPrecisionInt.java
@@ -29,10 +29,54 @@ public class IncrementAnArbitraryPrecisionInt {
     return arr;
   }
 
+  /* 
+   * Variant: Write a program which takes as input two strings s and t of bits encoding binary numbers
+   * Bs and Bt, respectively, and returns a new string of bits representing the number Bs + Bt
+   */
+  public static String plusBinary(String s, String t) {
+    int ps = s.length() - 1, pt = t.length() - 1;
+    String result = "";
+    int carry = 0;
+    while (ps >= 0 && pt >= 0) {
+      int cur = (s.charAt(ps) - '0') + (t.charAt(pt) - '0') + carry;
+      result = (cur == 1 || cur == 3 ? "1" : "0") + result;
+      carry = cur - 1 > 0 ? 1 : 0;
+      ps--; pt--;
+    }
+
+    while (ps >= 0) {
+      int cur = (s.charAt(ps) - '0') + carry;
+      result = (cur == 1 ? "1" : "0") + result;
+      carry = cur - 1 > 0 ? 1 : 0;
+      ps--;
+    }
+
+    while (pt >= 0) {
+      int cur = (t.charAt(pt) - '0') + carry;
+      result = (cur == 1 ? "1" : "0") + result;
+      carry = cur - 1 > 0 ? 1 : 0;
+      pt--;
+    }
+
+    if (carry > 0) {
+      result = "1" + result;
+    }
+
+    return result;
+  }
+
   public static void main(String[] args) {
     arrPrint(plusOne(new int[]{1,2,9}));
     arrPrint(plusOne(new int[]{9,9,9}));
     arrPrint(plusOne(new int[]{9,0,9}));
     arrPrint(plusOne(new int[]{0,0,0}));
+
+    System.out.println("===========================");
+
+    System.out.println(plusBinary("11111111","1"));
+    System.out.println(plusBinary("10001111","1"));
+    System.out.println(plusBinary("0000001","1"));
+    System.out.println(plusBinary("0000001","100000"));
+    System.out.println(plusBinary("0000001","100111"));
   }
 }
